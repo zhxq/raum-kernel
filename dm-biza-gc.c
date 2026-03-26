59d6a326c749d4b78f170faea65855357116e237
Xiangqun Zhang <xzhang84@syr.edu>
2026-03-26 16:19:54 -0400

    Print read

--- dm-biza-gc.c
+++ dm-biza-gc.c
@@ -242,8 +242,9 @@ static void biza_gc_work(struct work_struct *work)
 	if (WRITE_AMP_STAT &&
 	    ktime_get_boottime_ns() - atomic64_read(&bt->previous_print_time) >
 		    1000000000) {
-		pr_err("user_send %lld, data write %lld, parity write %lld, data in place update %lld, parity in place upate %lld, data flush %lld, parity flush %lld\n",
+		pr_err("user_send %lld, user_read %lld, data write %lld, parity write %lld, data in place update %lld, parity in place upate %lld, data flush %lld, parity flush %lld\n",
 		       atomic64_read(&bt->user_send),
+		       atomic64_read(&bt->user_read),
 		       atomic64_read(&bt->data_write),
 		       atomic64_read(&bt->parity_write),
 		       atomic64_read(&bt->data_in_place_update),
