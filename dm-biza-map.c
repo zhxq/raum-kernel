da00f22ea8e3ebb7fceaad7a49497d34b6afb18d
Xiangqun Zhang <zhxq@zhxqweb.com>
2026-04-20 23:50:47 -0400

    Use vmalloc

--- dm-biza-map.c
+++ dm-biza-map.c
@@ -627,8 +627,7 @@ int biza_ctr_map(struct biza_target *bt)
 		ret = -ENOMEM;
 	}
 
-	bt->map->l2p = kvmalloc_array(bt->params->nr_chunks,
-				      sizeof(biza_addr_t), GFP_KERNEL);
+	bt->map->l2p = vmalloc(bt->params->nr_chunks * sizeof(biza_addr_t));
 	if (!bt->map->l2p) {
 		pr_err("dm-biza: Failed to allocate l2p map\n");
 		ret = -ENOMEM;
@@ -637,9 +636,9 @@ int biza_ctr_map(struct biza_target *bt)
 	memset(bt->map->l2p, (uint8_t)BIZA_MAP_UNMAPPED,
 	       bt->params->nr_chunks * sizeof(biza_addr_t));
 
-	bt->map->p2l = kvmalloc_array(bt->params->nr_internal_chunks +
-					      bt->params->nr_total_raum_chunks,
-				      sizeof(biza_addr_t), GFP_KERNEL);
+	bt->map->p2l = vmalloc((bt->params->nr_internal_chunks +
+				bt->params->nr_total_raum_chunks) *
+			       sizeof(biza_addr_t));
 	if (!bt->map->p2l) {
 		pr_err("dm-biza: Failed to allocate p2l map\n");
 		ret = -ENOMEM;
